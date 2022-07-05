42d43e1d70a50407d2f9b10ec62effb8cd137541
Mumen Mansour <103611001+MumenMansour@users.noreply.github.com>
2022-07-05 10:31:43 -0300

    01

--- JOGO EM JS/carro.js
+++ JOGO EM JS/carro.js
@@ -1,30 +1,20 @@
-
-//carro 1
-let xcarro = 600;
-let ycarro = 40;
-let velocarro1 = 2;
-
-//carro2
-let xcarro2 = 600;
-let ycarro2 = 96;
-let velocarro2 = 2.5;
-
-//carro3 
-let xcarro3 = 600;
-let ycarro3 = 150;
-let velocarro3 = 3.2;
+//codigo de carros 
+//indice posicao 0-1-2 , começa sempre com o indice 0
+let ycarros = [40, 96 , 150];
+let velocarros = [2 , 2.5 , 3.2];
+let xcarros = [ 600 , 600, 600];
 
 function mostracarro(){
-  image(imgcarro1, xcarro, ycarro, 50, 40 );
-  image(imgcarro1, xcarro2, ycarro2, 50, 40 );
-  image(imgcarro1, xcarro3, ycarro3, 50, 40 );
+  for (let i = 0;  i < imgcarro.length; i = i+1 ) {
+  image(imgcarro[i], xcarros[i],ycarros[i], 50, 40 );
+  }
 }
 
 //velocidade do carro
 function movimentacarro(){
-  xcarro -= velocarro1 ;
-  xcarro2 -= velocarro2;
-  xcarro3 -= velocarro3;
+  xcarros-= velocarros[0];
+  xcarros-= velocarros[1];
+  xcarros-= velocarros[2];
 }
 
 //resetar os carros
@@ -40,3 +30,4 @@ function resetposicao(){
     xcarro3 = 600
   }
 }
+
